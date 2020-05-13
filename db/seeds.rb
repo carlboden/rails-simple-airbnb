6cf64f08f5df87b33df2691976419282a4c961fd
Carl Boden <carl_boden@hotmail.com>
2020-05-13 16:24:04 +0200

    bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light Flat Brussels',
+  address: '10 Rue de la Gare Brussels',
+  description: 'A lovely flat',
+  price_per_night: 70,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Dark Flat Brussels',
+  address: '14 Rue de la Loi Brussels',
+  description: 'A nice flat next to the Parliament',
+  price_per_night: 130,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Nice Appartment Brussels',
+  address: '11 Rue des Colonies Brussels',
+  description: 'A nice appartment full of light',
+  price_per_night: 110,
+  number_of_guests: 2
+)
